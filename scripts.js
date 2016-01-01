e3f367c24218141a556bf687fbb55326edecd154
EricR <rasliche@gmail.com>
2016-01-01 16:06:11 -0500

    checkForWin() function not working

--- scripts.js
+++ scripts.js
@@ -1,15 +1,23 @@
 // This is the logic for the Mancala game
 var MancalaGame = {
+  turn: 'A',
+
+  endTurn: function() {
+    this.checkForWin(turn);
+    turn = (turn === 'A') ? 'B' : 'A';
+  },
+
   board: {
     'A' : [4,4,4,4,4,4,0],
     'B' : [4,4,4,4,4,4,0]
   },
 
-  // Takes a side and a house index (0-5, 6 is the store)
+  // Takes a house index (0-5, 6 is the store)
   // Moves the gems
-  move: function(side, house) {
+  move: function(house) {
     var originHouse = house;
-    var originSide = side;
+    var originSide = this.turn;
+    var side = this.turn
     for (var gems = this.board[side][house]; gems > 0; gems -=1) {
       console.log("Entered for loop");
       console.log(gems+" "+house);
@@ -30,9 +38,23 @@ var MancalaGame = {
 
   // If a side is empty, the game is over and the sum of all gems on
   // a players side and in their store should result in a winner.
-  // It is possible to tie.
+  // It is possible to tie. Run check after a turn before changing
+  // sides
   checkForWin: function() {
-
+    var opp = this.turn === 'A' ? this.board['B'] : this.board['A'];
+    var total = 0;
+    for (var h = 0; h<6; h++) {
+      total+= this.board[this.turn][h];
+    }
+    if (total === 0) {
+      // If 'side' is empty, then we know their store (side[6]) is
+      // their total. opp.reduce() to get opp score, then compare
+      var oppTotal = opp.reduce(function(acc, c) {
+        return acc + c;
+      });
+      console.log(oppTotal);
+      this.board[this.turn][6] > oppTotal ? console.log(this.board[this.turn]) : console.log(opp);
+    } else { console.log("no winner")}
   },
 
   moveIsValid: function() {}
